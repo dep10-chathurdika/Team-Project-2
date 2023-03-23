c5bc49c7d75ada91e1a708d45eef97aa5b9a4113
tharuka-devinda <tharukadevinda96@gmail.com>
2023-03-23 18:22:05 +0530

    UI finished

---
+++ src/main/java/lk/ijse/dep10/app/ManageEmployeeController.java
@@ -0,0 +1,47 @@
+package lk.ijse.dep10.app;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ManageEmployeeController {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNewEmployee;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private TableView<?> tblEmployees;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewEmployeeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+}
