8f20ee03518e3affc7d99c67a0d2cc40dffb62ae
tharuka-devinda <tharukadevinda96@gmail.com>
2023-03-23 19:49:02 +0530

    initialize method done

--- src/main/java/lk/ijse/dep10/app/controller/ManageEmployeeController.java
+++ src/main/java/lk/ijse/dep10/app/controller/ManageEmployeeController.java
@@ -2,10 +2,14 @@ package lk.ijse.dep10.app.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import lk.ijse.dep10.app.db.DBConnection;
 import lk.ijse.dep10.app.model.Employee;
+
+import java.sql.*;
 
 public class ManageEmployeeController {
 
@@ -30,6 +34,44 @@ public class ManageEmployeeController {
     @FXML
     private TextField txtName;
 
+    public void initialize() {
+
+        tblEmployees.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblEmployees.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblEmployees.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
+
+        loadAllEmployees();
+
+        tblEmployees.getSelectionModel().selectedItemProperty().addListener((ov, prev, current) -> {
+            btnDelete.setDisable(current == null);
+            if (current == null) return;
+
+            txtID.setText(current.getId() + "");
+            txtName.setText(current.getName());
+            txtAddress.setText(current.getAddress());
+        });
+    }
+
+    private void loadAllEmployees() {
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            Statement stm = connection.createStatement();
+            ResultSet rst = stm.executeQuery("SELECT * FROM Employee");
+
+            while (rst.next()) {
+                String id = rst.getString("employee_id");
+                String name = rst.getString("employee_name");
+                String address = rst.getString("employee_address");
+
+                Employee employee = new Employee(id, name, address);
+                tblEmployees.getItems().add(employee);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to load employee details. Try again!", ButtonType.OK).show();
+        }
+    }
+
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
 
@@ -37,7 +79,17 @@ public class ManageEmployeeController {
 
     @FXML
     void btnNewEmployeeOnAction(ActionEvent event) {
+        txtName.clear();
+        txtName.clear();
+        txtAddress.clear();
 
+        txtID.setDisable(false);
+        txtName.setDisable(false);
+        txtAddress.setDisable(false);
+        btnSave.setDisable(false);
+
+        generateID();
+        txtName.requestFocus();
     }
 
     @FXML
@@ -52,6 +104,25 @@ public class ManageEmployeeController {
             newID++;
             txtID.setText(String.format("E%03d", newID));
         }
+    }
+
+    private boolean isDataValid() {
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        boolean dataValid = true;
+
+        if (!name.matches("[A-z ]+")) {
+            txtName.requestFocus();
+            txtName.selectAll();
+            dataValid = false;
+        }
+
+        if (!address.matches("[A-z ]+")) {
+            txtAddress.requestFocus();
+            txtAddress.selectAll();
+            dataValid = false;
+        }
+        return dataValid;
     }
 
 }
