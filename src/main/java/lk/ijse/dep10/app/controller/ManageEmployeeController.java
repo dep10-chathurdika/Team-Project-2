a0ac9ac4cea3b9ad864100bce24e0a707fb5c7e9
tharuka-devinda <tharukadevinda96@gmail.com>
2023-03-23 19:30:52 +0530

    Employee.class complete

--- src/main/java/lk/ijse/dep10/app/controller/ManageEmployeeController.java
+++ src/main/java/lk/ijse/dep10/app/controller/ManageEmployeeController.java
@@ -1,10 +1,11 @@
-package lk.ijse.dep10.app;
+package lk.ijse.dep10.app.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import lk.ijse.dep10.app.model.Employee;
 
 public class ManageEmployeeController {
 
@@ -18,7 +19,7 @@ public class ManageEmployeeController {
     private Button btnSave;
 
     @FXML
-    private TableView<?> tblEmployees;
+    private TableView<Employee> tblEmployees;
 
     @FXML
     private TextField txtAddress;
@@ -42,6 +43,15 @@ public class ManageEmployeeController {
     @FXML
     void btnSaveOnAction(ActionEvent event) {
 
+    }
+
+    private void generateID() {
+        if (tblEmployees.getItems().size() == 0) txtID.setText("E001");
+        else {
+            int newID = Integer.parseInt(tblEmployees.getItems().get(tblEmployees.getItems().size() - 1).getId().substring(1));
+            newID++;
+            txtID.setText(String.format("E%03d", newID));
+        }
     }
 
 }
