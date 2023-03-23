c61676aa74c95a2250e0f03d28c49c8b669745f8
Dinindu Lokugamage <dinindu961030@gmail.com>
2023-03-23 18:38:11 +0530

    dinindu 1

---
+++ src/main/java/lk/ijse/dep10/app/controller/TeachersViewController.java
@@ -0,0 +1,53 @@
+package lk.ijse.dep10.app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class TeachersViewController {
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNewTeacher;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private TableView<?> tblTeachers;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewTeacherOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tblTeachersOnKeyReleased(KeyEvent event) {
+
+    }
+
+}
