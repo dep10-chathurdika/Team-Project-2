17ae4c38988f0c5d13d7a65365925d1dff0b1bec
Chathurdika Dayarathna <chathurdika1dayarathna@gmail.com>
2023-03-23 18:37:23 +0530

    Basics completed

---
+++ src/main/java/lk/ijse/dep10/app/controller/MainViewController.java
@@ -0,0 +1,41 @@
+package lk.ijse.dep10.app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainViewController {
+
+    @FXML
+    private Button btnMngCustomers;
+
+    @FXML
+    private Button btnMngEmployees;
+
+    @FXML
+    private Button btnMngStudents;
+
+    @FXML
+    private Button btnMngTeachers;
+
+    @FXML
+    void btnMngCustomersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMngEmployeesOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMngStudentsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMngTeachersOnAction(ActionEvent event) {
+
+    }
+
+}
