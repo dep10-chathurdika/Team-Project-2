94de080bcff44bf5d4afb1064b87a2dea80522d9
Chethini Kobewatta <ckavindya@gmail.com>
2023-03-23 18:38:26 +0530

    chethini 1

---
+++ src/main/java/AppInitializerWrapper.java
@@ -0,0 +1,7 @@
+import lk.ijse.dep10.app.AppInitializer;
+
+public class AppInitializerWrapper {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
---
+++ src/main/java/lk/ijse/dep10/app/controller/StudentsViewController.java
@@ -0,0 +1,29 @@
+package lk.ijse.dep10.app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class StudentsViewController {
+    public Button btnNewStudent;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public Button btnSave;
+    public Button btnDelete;
+    public TableView tblStudents;
+
+    public void btnNewStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void tblStudentsOnKeyReleased(KeyEvent keyEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+}
